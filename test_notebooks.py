7b17e19ba7221aeae68685922b83020ead9f8927
Miranda Mundt <mmundt@sandia.gov>
2025-03-10 12:16:23 -0600

    Update copyright; add to all *.py files

--- test_notebooks.py
+++ test_notebooks.py
@@ -1,3 +1,15 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2015-2025
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
+
 #
 # Jupyter notebook testing logic adapted from
 #   https://gist.github.com/lheagy/f216db7220713329eb3fc1c2cd3c7826
